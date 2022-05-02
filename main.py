adc8adf7681727db071542bb3100f28e5bc9ef50
Marcel Jerzyk <244979@student.pwr.edu.pl>
2022-05-02 11:57:47 +0200

    A: Auto branch

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from rich import print
+from git.repo import Repo
 
 __version__ = "0.5.0"
+
+
+def do_versioning(branch_name: str):
+    repo = Repo('.')
+    branch = repo.create_head(branch_name)
+    branch.checkout()
+    repo.git.commit("--allow-empty", "-m", f'A: Feature #1 ({branch_name})')
+    repo.git.commit("--allow-empty", "-m", f'A: Feature #2 ({branch_name})')
+    repo.git.commit("--allow-empty", "-m", f'A: Feature #3 ({branch_name})')
+    repo.git.commit("--allow-empty", "-m", f'R: Something #4 ({branch_name})')
+    repo.git.commit("--allow-empty", "-m", f'R: Something #5 ({branch_name})')
+    repo.remote("origin").push()
 
 
 def main():
